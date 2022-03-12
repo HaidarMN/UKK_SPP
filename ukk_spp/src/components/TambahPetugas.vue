e11e372096b6d39f8f53201a22a35e53c753053e
HMNaufal <55501917+HaidarMN@users.noreply.github.com>
2022-03-12 23:49:34 +0700

    Add files via upload

---
+++ ukk_spp/src/components/TambahPetugas.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="content">
+        <!-- Form -->
+        <div class="add-form">
+            Nama
+            <input type="text" name="nama_petugas" class="form-control" v-model="nama_petugas" placeholder="Masukkan nama" 
+            autocomplete="off">
+            <br>
+            Username
+            <input type="text" name="username" class="form-control" v-model="username" placeholder="Masukkan username"
+            autocomplete="off">
+            <br>
+            E-mail
+            <input type="email" name="email" class="form-control" v-model="email" placeholder="Masukkan email"
+            autocomplete="off">
+            <br>
+            Password
+            <input type="password" name="password" class="form-control" v-model="password" placeholder="Masukkan password" 
+            autocomplete="off">
+            <br>
+            Level
+            <select name="level" class="form-control" v-model="level">
+                <option value="" selected hidden disabled>Pilih level</option>
+                <option value="admin">Admin</option>
+                <option value="petugas">Petugas</option>
+            </select>
+            <br>
+
+            <!-- Button -->
+            <button class="button-add" @click="addpetugas()">
+                <i class='bx bxs-file-plus button-icon'></i>
+                <span class="button-text">Add</span>
+            </button>
+            <router-link class="button-back" to="/petugas">
+                <i class='bx bxs-chevron-left button-icon'></i>
+                <span class="button-text">Back</span>
+            </router-link>
+
+            <!-- Notification -->
+            <br><br>
+            <div v-bind:class="style_msg">
+                <div v-if="error == true">
+                    <div v-for="msg in message" :key="msg.id">
+                        <p v-for="m in msg" :key="m.id">{{m}}</p>
+                    </div>
+                </div>
+                <p v-else>{{message}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TambahPetugas",
+        data() {
+            return {
+                nama_petugas:'',
+                username:'',
+                email:'',
+                password:'',
+                level:'',
+                id_petugas:'',
+                style_msg: '',
+                message: '',
+                error:false
+            }
+        },
+
+        methods: {
+            addpetugas:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                var datapetugas = {
+                    username:this.username,
+                    password:this.password,
+                    nama_petugas:this.nama_petugas
+                }
+
+                this.axios.post("http://localhost/lat_spp/public/api/insert_petugas", datapetugas, option).then((result) => {
+                    if(result.data.status == true) {
+                        // console.log(result)
+                        this.error = false
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-success"
+
+                        // Pergi ke halaman sebelumnya setelah 2000ms
+                        setTimeout(() => {
+                            this.$router.push('/petugas')
+                        }, 2000);
+                    } else {
+                        this.error = true
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-danger"
+                    }
+                })
+
+                var datauser = {
+                    name:this.nama_petugas,
+                    email:this.email,
+                    password:this.password,
+                    level:this.level,
+                    id_petugas:this.id_petugas
+                }
+
+                this.axios.post("http://localhost/lat_spp/public/api/insert_user", datauser, option).then((result) => {
+                    // console.log(result)
+                    if(result.data.status == true) {
+                        this.error = false
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-success"
+
+                        // Pergi ke halaman sebelumnya setelah 2000ms
+                        setTimeout(() => {
+                            this.$router.push('/petugas')
+                        }, 2000);
+                    } else {
+                        this.error = true
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-danger"
+                    }
+                })
+            }
+        }
+    }
+</script>
---
+++ ukk_spp/src/components/TambahSpp.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="content">
+        <!-- Form -->
+        <div class="add-form">
+            Angkatan
+            <input type="number" name="angkatan" class="form-control" v-model="angkatan" placeholder="Masukkan angkatan" 
+            autocomplete="off">
+            <br>
+            Tahun
+            <input type="number" name="tahun" class="form-control" v-model="tahun" placeholder="Masukkan tahun" 
+            autocomplete="off">
+            <br>
+            Nominal
+            <input type="number" name="nominal" class="form-control" v-model="nominal" placeholder="Masukkan nominal" 
+            autocomplete="off">
+            <br>
+
+            <!-- Button -->
+            <button class="button-add" @click="addspp()">
+                <i class='bx bxs-file-plus button-icon'></i>
+                <span class="button-text">Add</span>
+            </button>
+            <router-link class="button-back" to="/spp">
+                <i class='bx bxs-chevron-left button-icon'></i>
+                <span class="button-text">Back</span>
+            </router-link>
+
+            <!-- Notification -->
+            <br><br>
+            <div v-bind:class="style_msg">
+                <div v-if="error == true">
+                    <div v-for="msg in message" :key="msg.id_spp">
+                        <p v-for="m in msg" :key="m.id_spp">{{m}}</p>
+                    </div>
+                </div>
+                <p v-else>{{message}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TambahSpp",
+        data() {
+            return {
+                angkatan:'',
+                tahun:'',
+                nominal: '',
+                message: '',
+                error:false
+            }
+        },
+
+        methods: {
+            addspp:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                var dataspp = {
+                    angkatan:this.angkatan,
+                    tahun:this.tahun,
+                    nominal:this.nominal
+                }
+
+                this.axios.post("http://localhost/lat_spp/public/api/insert_spp", dataspp, option).then((result) => {
+                    // console.log(result)
+                    if(result.data.status == true) {
+                        this.error = false
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-success"
+
+                        // Pergi ke halaman sebelumnya setelah 2000ms
+                        setTimeout(() => {
+                            this.$router.push('/spp')
+                        }, 2000);
+                    } else {
+                        this.error = true
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-danger"
+                    }
+                })
+            }
+        }
+    }
+</script>
