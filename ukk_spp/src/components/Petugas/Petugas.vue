9e193a6f8089feef14652176abd72fef746ad190
HMNaufal <55501917+HaidarMN@users.noreply.github.com>
2022-03-17 10:34:50 +0700

    Add files via upload

---
+++ ukk_spp/src/components/Petugas/Petugas.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="content">
+        <h1 >DATA PETUGAS</h1>
+
+        <!-- Add | Search | Button -->
+        <div class="grid">
+            <input type="text" name="search" v-model="search" class="form-control grid-left" placeholder="Search..." 
+            @keyup.enter="cari()" autocomplete="off">
+            <router-link class="button-add grid-right" to="/petugas/tambahpetugas">
+                <i class='bx bxs-file-plus button-icon'></i>
+                <span class="button-text">Add</span>
+            </router-link>
+        </div>
+
+        <!-- Table -->
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>E-mail</th>
+                    <th>Level</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="us in user" :key="us.id">
+                    <td>{{us.id}}</td>
+                    <td>{{us.name}}</td>
+                    <td>{{us.email}}</td>
+                    <td>{{us.level}}</td>
+                    <td>
+                        <router-link class="button-edit" :to="{path: '/petugas/editpetugas/' + us.id}">
+                            <i class='bx bxs-edit-alt button-icon'></i>
+                        </router-link>
+                        &nbsp;
+                        <a class="button-del" @click="delpetugas(us.id)">
+                            <i class='bx bxs-trash button-icon'></i>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        data() {
+            return {
+                user:[],
+                search:''
+            }
+        },
+
+        methods: {
+            cari:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                var data_cari = ""
+                if(this.search == "") {
+                    data_cari = ""
+                } else {
+                    data_cari = "/" + this.search
+                }
+
+                this.axios.get("http://localhost/lat_spp/public/api/user" + data_cari, option).then((result) => {
+                    // console.log(result)
+                    this.user = result.data
+                })
+            },
+
+            getpetugas:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                this.axios.get("http://localhost/lat_spp/public/api/user", option).then((result) => {
+                    // console.log(result)
+                    this.user = result.data
+                })
+            },
+
+            delpetugas:function(id) {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                if(confirm('yakin?')) {
+                    this.axios.delete("http://localhost/lat_spp/public/api/delete_user/" + id, option).then((result) => {
+                        console.log(result)
+                        this.getpetugas()
+                    })
+                }
+            }
+        },
+
+        mounted() {
+            this.getpetugas()
+        }
+    }
+</script>
---
+++ ukk_spp/src/components/Petugas/TambahPetugas.vue
@@ -0,0 +1,137 @@
+<template>
+    <div class="content">
+        <!-- Form -->
+        <div class="add-form">
+            Nama
+            <input type="text" name="nama_petugas" class="form-control" v-model="nama_petugas" placeholder="Masukkan nama" 
+            autocomplete="off">
+            <br>
+            Username
+            <input type="text" name="username" class="form-control" v-model="username" placeholder="Masukkan username"
+            autocomplete="off">
+            <br>
+            E-mail
+            <input type="email" name="email" class="form-control" v-model="email" placeholder="Masukkan email"
+            autocomplete="off">
+            <br>
+            Password
+            <input type="password" name="password" class="form-control" v-model="password" placeholder="Masukkan password" 
+            autocomplete="off">
+            <br>
+            Password Confirmation
+            <input type="password" name="password_confirmation" class="form-control" v-model="password_confirmation" 
+            placeholder="Ulangi password" autocomplete="off">
+            <br>
+            Level
+            <select name="level" class="form-control" v-model="level">
+                <option value="" selected hidden disabled>Pilih level</option>
+                <option value="admin">Admin</option>
+                <option value="petugas">Petugas</option>
+            </select>
+            <br>
+
+            <!-- Button -->
+            <router-link class="button-back" to="/petugas">
+                <i class='bx bxs-chevron-left button-icon'></i>
+                <span class="button-text">Back</span>
+            </router-link>
+            <button class="button-add" @click="addpetugas()">
+                <i class='bx bxs-file-plus button-icon'></i>
+                <span class="button-text">Add</span>
+            </button>
+
+            <!-- Notification -->
+            <br><br>
+            <div v-bind:class="style_msg">
+                <div v-if="error == true">
+                    <div v-for="msg in message" :key="msg.id">
+                        <p v-for="m in msg" :key="m.id">{{m}}</p>
+                    </div>
+                </div>
+                <p v-else>{{message}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TambahPetugas",
+        data() {
+            return {
+                nama_petugas:'',
+                username:'',
+                email:'',
+                password:'',
+                password_confirmation:'',
+                level:'',
+                id_petugas:'',
+                style_msg: '',
+                message: '',
+                error:false
+            }
+        },
+
+        methods: {
+            addpetugas:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                var datapetugas = {
+                    username:this.username,
+                    password:this.password,
+                    nama_petugas:this.nama_petugas
+                }
+
+                this.axios.post("http://localhost/lat_spp/public/api/insert_petugas", datapetugas, option).then((result) => {
+                    if(result.data.status == true) {
+                        // console.log(result)
+                        this.error = false
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-success"
+
+                        // Pergi ke halaman sebelumnya setelah 2000ms
+                        setTimeout(() => {
+                            this.$router.push('/petugas')
+                        }, 2000);
+                    } else {
+                        this.error = true
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-danger"
+                    }
+                })
+
+                var datauser = {
+                    nama_petugas:this.nama_petugas,
+                    username:this.username,
+                    email:this.email,
+                    password:this.password,
+                    password_confirmation:this.password_confirmation,
+                    level:this.level,
+                    id_petugas:this.id_petugas
+                }
+
+                this.axios.post("http://localhost/lat_spp/public/api/register", datauser, option).then((result) => {
+                    console.log(result)
+                    if(result.data.status == true) {
+                        this.error = false
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-success"
+
+                        // Pergi ke halaman sebelumnya setelah 2000ms
+                        setTimeout(() => {
+                            this.$router.push('/petugas')
+                        }, 2000);
+                    } else {
+                        this.error = true
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-danger"
+                    }
+                })
+            }
+        }
+    }
+</script>
