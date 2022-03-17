f2eec96dda99d5f96079118b77edd188e922fd5a
HMNaufal <55501917+HaidarMN@users.noreply.github.com>
2022-03-17 10:32:46 +0700

    Add files via upload

---
+++ ukk_spp/src/components/Kelas/EditKelas.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="content">
+        <!-- Form -->
+        <div class="add-form">
+            <input type="hidden" name="id_kelas" v-model="id_kelas" class="form-control">
+            Nama Kelas
+            <input type="text" name="nama_kelas" class="form-control" v-model="nama_kelas" placeholder="Masukkan nama" 
+            autocomplete="off">
+            <br>
+            Jurusan
+            <select name="jurusan" class="form-control" v-model="jurusan">
+                <option value="" selected hidden disabled>Pilih jurusan</option>
+                <option value="RPL">Rekayasa Perangkat Lunak</option>
+                <option value="TKJ">Teknik Komputer Jaringan</option>
+            </select>
+            <br>
+            Angkatan
+            <input type="number" name="angkatan" class="form-control" v-model="angkatan" placeholder="Masukkan angkatan" 
+            autocomplete="off">
+            <br>
+
+            <!-- Button -->
+            <button class="button-edit" @click="editkelas()">
+                <i class='bx bxs-edit-alt button-icon'></i>
+                <span class="button-text">Edit</span>
+            </button>
+            <router-link class="button-back" to="/kelas">
+                <i class='bx bxs-chevron-left button-icon'></i>
+                <span class="button-text">Back</span>
+            </router-link>
+
+            <!-- Notification -->
+            <br><br>
+            <div v-bind:class="style_msg">
+                <div v-if="error == true">
+                    <div v-for="msg in message" :key="msg.id_kelas">
+                        <p v-for="m in msg" :key="m.id_kelas">{{m}}</p>
+                    </div>
+                </div>
+                <p v-else>{{message}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EditKelas",
+        data() {
+            return {
+                id_kelas:'',
+                nama_kelas:'',
+                jurusan:'',
+                angkatan:'',
+                style_msg: '',
+                message: '',
+                error:false
+            }
+        },
+
+        methods: {
+            getdetail(id_kelas) {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                this.axios.get("http://localhost/lat_spp/public/api/getkelas/" + id_kelas, option).then((result) => {
+                    // console.log(result)
+                    this.id_kelas   = result.data.id_kelas,
+                    this.nama_kelas = result.data.nama_kelas,
+                    this.jurusan    = result.data.jurusan,
+                    this.angkatan   = result.data.angkatan
+                })
+            },
+
+            editkelas:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                var datakelas = {
+                    nama_kelas:this.nama_kelas,
+                    jurusan:this.jurusan,
+                    angkatan:this.angkatan
+                }
+
+                this.axios.put("http://localhost/lat_spp/public/api/update_kelas/" + this.id_kelas, datakelas, option).then((result) => {
+                    console.log(result)
+                    if(result.data.status == true) {
+                        this.error = false
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-success"
+
+                        // Pergi ke halaman sebelumnya setelah 2000ms
+                        setTimeout(() => {
+                            this.$router.push('/kelas')
+                        }, 2000);
+                    } else {
+                        this.error = true
+                        this.message = result.data.message
+                        this.style_msg = "alert alert-danger"
+                    }
+                })
+            }
+        },
+
+        mounted() {
+            this.getdetail(this.$route.params.id_kelas)
+        }
+    }
+</script>
---
+++ ukk_spp/src/components/Kelas/Kelas.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="content">
+        <h1 >DATA KELAS</h1>
+
+        <!-- Add | Search | Button -->
+        <div class="grid">
+            <input type="text" name="search" v-model="search" class="form-control grid-left" placeholder="Search..." 
+            @keyup.enter="cari()" autocomplete="off">
+            <router-link class="button-add grid-right" to="/kelas/tambahkelas">
+                <i class='bx bxs-file-plus button-icon'></i>
+                <span class="button-text">Add</span>
+            </router-link>
+        </div>
+
+        <!-- Table -->
+        <table class="content-table">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nama</th>
+                    <th>Jurusan</th>
+                    <th>Angkatan</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="kel in kelas" :key="kel.id_kelas">
+                    <td>{{kel.id_kelas}}</td>
+                    <td>{{kel.nama_kelas}}</td>
+                    <td>{{kel.jurusan}}</td>
+                    <td>{{kel.angkatan}}</td>
+                    <td>
+                        <router-link class="button-edit" :to="{path: '/kelas/editkelas/' + kel.id_kelas}">
+                            <i class='bx bxs-edit-alt button-icon'></i>
+                        </router-link>
+                        &nbsp;
+                        <a class="button-del" @click="delkelas(kel.id_kelas)">
+                            <i class='bx bxs-trash button-icon'></i>
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Kelas",
+        data() {
+            return {
+                kelas:[],
+                search:'',
+            }
+        },
+
+        methods: {
+            cari:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                var data_cari = ""
+                if(this.search == "") {
+                    data_cari = ""
+                } else {
+                    data_cari = "/" + this.search
+                }
+
+                this.axios.get("http://localhost/lat_spp/public/api/kelas" + data_cari, option).then((result) => {
+                    // console.log(result)
+                    this.kelas = result.data
+                })
+            },
+
+            getkelas:function() {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                this.axios.get("http://localhost/lat_spp/public/api/kelas", option).then((result) => {
+                    // console.log(result)
+                    this.kelas = result.data
+                })
+            },
+
+            delkelas:function(id_kelas) {
+                var option = {
+                    headers:{
+                        'Authorization':'bearer ' + localStorage.getItem("token")
+                    }
+                }
+
+                if(confirm('yakin?')) {
+                    this.axios.delete("http://localhost/lat_spp/public/api/delete_kelas/" + id_kelas, option).then((result) => {
+                        console.log(result)
+                        this.getkelas()
+                    })
+                }
+            }
+        },
+
+        mounted() {
+            this.getkelas()
+        }
+    }
+</script>
