2c28517d915611bf00c89bdfd8373e1375abbb68
HMNaufal <55501917+HaidarMN@users.noreply.github.com>
2022-04-01 15:31:10 +0700

    Add files via upload

--- lat_spp/app/Http/Controllers/PembayaranController.php
+++ lat_spp/app/Http/Controllers/PembayaranController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Pembayaran;
 use Illuminate\Support\Facades\validator;
+use JWTAuth;
 use Auth;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class PembayaranController extends Controller
 {
@@ -14,27 +16,44 @@ class PembayaranController extends Controller
     //POST
     public function store (Request $req) {
         $validator = Validator::make($req->all(), [
-            'id_petugas'        => 'required',
             'nisn'              => 'required',
-            'tgl_bayar'         => 'required',
             'bulan_spp'         => 'required',
             'tahun_spp'         => 'required',
         ]);
+
         if($validator->fails()) {
-            return Response ()->json($validator->errors());
+            $data['status']     = false;
+            $data['message']   = $validator->errors();
+            return Response ()->json($data);
         }
-        $save = Pembayaran::create([
-            'id_petugas'        => $req->id_petugas,
-            'nisn'              => $req->nisn,
-            'tgl_bayar'         => $req->tgl_bayar,
-            'bulan_spp'         => $req->bulan_spp,
-            'tahun_spp'         => $req->tahun_spp,
-        ]);
-        if($save) {
-            return Response()->json(['status'=>'Berhasil menambah pembayaran']);
+
+        $ceklunas =  Pembayaran::where('nisn', $req->get('nisn'))
+                                ->where('bulan_spp', $req->get('bulan_spp'))
+                                ->where('tahun_spp', $req->get('tahun_spp'));
+        
+        if($ceklunas->count() == 0) {
+            $pembayaran = Pembayaran::create([
+                'id_petugas'    => JWTAuth::user()->id_petugas,
+                'nisn'          => $req->get('nisn'),
+                'tgl_bayar'     => date('Y-m-d'),
+                'bulan_spp'     => $req->get('bulan_spp'),
+                'tahun_spp'     => $req->get('tahun_spp'),
+                'status'        => 'lunas'
+            ]);
+
+            if($pembayaran) {
+                $data['status']     = true;
+                $data['message']    = "Pembayaran berhasil";
+            } else {
+                $data['status']     = false;
+                $data['message']    = "Pembayaran gagal";
+            }
+            return $data;
         } else {
-            return Response()->json(['status'=>'Gagal menambah pembayaran']);
+            $data['status']     = false;
+            $data['message']    = "SPP bulan ini sudah lunas";
         }
+        return $data;
     }
     
     //UPDATE
@@ -99,7 +118,7 @@ class PembayaranController extends Controller
 
     //LAST
     public function last() {
-        $last = DB::table('pembayaran')->latest('id_pembayaran')->take(5)->get();
+        $last = DB::table('pembayaran')->latest('id_pembayaran')->take(4)->get();
         return Response()->json($last);
     }
 
